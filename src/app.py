5523d06f76b2ae17d842c2dea5fa1bec7bd9e51f
cristian <phaser@tubatuba.net>
2023-02-15 13:55:48 +0100

    Upload traced svg files to S3 CU-863fxa5vq

--- src/app.py
+++ src/app.py
@@ -3,11 +3,19 @@ from flask import Flask, request
 from process_binary import process as process_binary
 from markup import build_markup
 from read import ReadError, ChannelCountError, SizeError
+import boto3
+import cuid
+import json
+import check_variables
+
+check_variables(); # Halt if the required environment variables are not defined
 
 app = Flask(__name__)
 
 NO_URL_ERROR = 'Image URL not provided.'
 PORT = os.environ['PORT']
+S3_UPLOADS_BUCKET = os.environ['VECTORIZING_S3_BUCKET']
+S3 = boto3.client("s3")
 
 @app.route('/', methods = ['GET'])
 def process():
@@ -27,8 +35,14 @@ def process():
             img_width,
             img_height
         )
-        
-        return markup
+        cuidStr = cuid.cuid()
+        S3.put_object(
+            Body=markup.encode('utf-8'),
+            Bucket=S3_UPLOADS_BUCKET,
+            Key=cuidStr,
+            ContentType="image/svg+xml",
+        )
+        return json.dumps({ "objectId": cuidStr })
     
     except (ReadError, ChannelCountError, SizeError) as e:
         return str(e), 400
